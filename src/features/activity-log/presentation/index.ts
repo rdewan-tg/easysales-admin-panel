f6913cf0ba9f23c2e0b08b3aeab8505782187698
Richard Dewan <richarddewan.tg@gmail.com>
2025-05-06 17:55:33 +0530

    fix the pagination in activity log screen

--- src/features/activity-log/presentation/index.ts
+++ src/features/activity-log/presentation/index.ts
@@ -1,3 +1,3 @@
 export * from "./state/activity-log-state";
 export { default as useActivityLogStore } from "./store/activity-log-store";
-export * from "./ui/ActivityLogScreen";
+export { default as ActivityLogScreen } from "./ui/ActivityLogScreen";
